c6aedf05127cfa9e95619fb211940b4d2fd7918f
Udayanzoysa <unzoysa.un@gmail.com>
2024-10-25 10:26:47 +0530

    checking ip address

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Flask on dcrypto.site!"
+    return "Hello, Flask!"
 
-if __name__ == "__main__":
-    app.run(host="http://145.223.19.67/", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
